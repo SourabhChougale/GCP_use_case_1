eec8bcf166694a9cb6cc50171977138f70374962
SourabhChougale <99398979+SourabhChougale@users.noreply.github.com>
2022-03-09 14:34:43 +0530

    Update Options.java

--- GCP_LAB_1/src/main/java/Options.java
+++ GCP_LAB_1/src/main/java/Options.java
@@ -4,15 +4,15 @@ public interface Options extends DataflowPipelineOptions {
 
 
     @Description("BigQuery table name")
-    String getOutputTableName();
-    void setOutputTableName(String outputTable);
+    String getoutputTable();
+    void setoutputTableName(String outputTable);
 
     @Description("Input topic name")
-    String getInputTopic();
-    void setInputTopic(String inputTopic);
+    String getinputTopic();
+    void setinputTopic(String inputTopic);
 
 
     @Description("The DLQ Topic used to store malformed Messages.")
-    String getDLQTopic();
-    void setDLQTopic(String dlqTopic);
+    String getdlqTopic();
+    void setdlqTopic(String dlqTopic);
 }
