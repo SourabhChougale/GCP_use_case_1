f9915c34a0d84de51b4c837d9b1ce4269a8c40c5
SourabhChougale <99398979+SourabhChougale@users.noreply.github.com>
2022-03-10 14:48:37 +0530

    Update Demo.java

--- GCP_LAB_1/src/main/java/Demo.java
+++ GCP_LAB_1/src/main/java/Demo.java
@@ -14,31 +14,42 @@
  * limitations under the License.
  */
 
+import com.google.api.services.bigquery.model.TableRow;
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.PipelineResult;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
+import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.schemas.JavaFieldSchema;
+import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.schemas.annotations.DefaultSchema;
 import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.JsonToRow;
+import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.*;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 public class Demo {
-
+    static final TupleTag<TableRow> parsedMessages = new TupleTag<TableRow>() {
+    };
+    static final TupleTag<String> unparsedMessages = new TupleTag<String>() {
+    };
     /**
      * The logger to output status messages to.
      */
     private static final Logger LOG = LoggerFactory.getLogger(Demo.class);
 
     /**
-     * The {@link Options} class provides the custom execution options passed by the
+     * The {@link Option} class provides the custom execution options passed by the
      * executor at the command-line.
      */
     public interface Option extends DataflowPipelineOptions {
@@ -75,27 +86,59 @@ public class Demo {
      * A class used for parsing JSON web server events
      * Annotated with @DefaultSchema to the allow the use of Beam Schemas and <Row> object
      */
-    @DefaultSchema(JavaFieldSchema.class)
+   /* @DefaultSchema(JavaFieldSchema.class)
     public static class CommonLog {
         int id;
         String name;
         String surname;
 
-    }
+    }*/
 
     /**
      * A DoFn acccepting Json and outputing CommonLog with Beam Schema
      */
-    static class JsonToCommonLog extends DoFn<String, CommonLog> {
+ /*   static class JsonToCommonLog extends DoFn<String, CommonLog> {
 
-        @ProcessElement
-        public void processElement(@Element String json, OutputReceiver<CommonLog> r) throws Exception {
-            Gson gson = new Gson();
-            CommonLog commonLog = gson.fromJson(json, CommonLog.class);
-            r.output(commonLog);
+       @ProcessElement
+       public void processElement(@Element String json, OutputReceiver<CommonLog> r) throws JsonSyntaxException {
+            try {
+               // Gson gson = new Gson();
+                CommonLog commonLog = new Gson().fromJson(json, CommonLog.class);
+                r.output(commonLog);
+            }catch(JsonSyntaxException e){
+                e.printStackTrace();
+            }
+
+
+        }
+    }*/
+    public static class PubsubMessageToRow extends PTransform<PCollection<String>, PCollectionTuple> {
+        @Override
+        public PCollectionTuple expand(PCollection<String> input) {
+            return input
+                    .apply("JsonToRow", ParDo.of(new DoFn<String, TableRow>() {
+                                @ProcessElement
+                                public void processElement(ProcessContext context) {
+                                    String json = context.element();
+                                    JSONObject obj=new JSONObject(json);
+
+                                    try {
+                                        TableRow row=new TableRow().
+                                                set("id",obj.getInt("id")).
+                                                set("name",obj.getString("name")).
+                                                set("surname",obj.getString("surname"));
+                                        context.output(parsedMessages,row);
+                                    } catch (JsonSyntaxException e) {
+                                        context.output(unparsedMessages, json);
+                                    }
+
+                                }
+                            })
+                            .withOutputTags(parsedMessages, TupleTagList.of(unparsedMessages)));
+
+
         }
     }
-
     /**
      * Runs the pipeline to completion with the specified options. This method does
      * not wait until the pipeline is finished before returning. Invoke
@@ -105,29 +148,45 @@ public class Demo {
      * @param options The execution options.
      * @return The pipeline result.
      */
+ /*   public static final Schema rawSchema = Schema
+            .builder()
+            .addInt32Field("id")
+            .addStringField("name")
+            .addStringField("surname")
+            .build();
+*/
+
     public static PipelineResult run(Option options) {
 
-        // Create the pipeline
-        Pipeline pipeline = Pipeline.create(options);
+    // Create the pipeline
+    Pipeline pipeline = Pipeline.create(options);
 
 
-        // Static input and output
-        String input = options.getinputTopic();
-        String output = options.getoutputTable();
+    // Static input and output
+    String input = options.getinputTopic();
+    String output = options.getoutputTable();
 
-        /*
-         * Steps:
-         * 1) Read something
-         * 2) Transform something
-         * 3) Write something
-         */
+    /*
+     * Steps:
+     * 1) Read something
+     * 2) Transform something
+     * 3) Write something
+     */
 
-        pipeline.apply("ReadFromGCS", TextIO.read().from(input))
-                .apply("ParseJson", ParDo.of(new JsonToCommonLog()))
-                .apply("WriteToBQ",
-                        BigQueryIO.<CommonLog>write().to(output).useBeamSchema()
-                                .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND)
-                                .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED));
+        PCollectionTuple  pubsub=pipeline.apply("ReadFromGCS", PubsubIO.readStrings().fromTopic(input))
+                .apply("ConvertMessageToCommonLog", new PubsubMessageToRow());
+
+
+
+       pubsub.get(parsedMessages)
+               .apply("WriteToBQ", BigQueryIO.<TableRow>write().to(output).useBeamSchema()
+                       .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_APPEND)
+                       .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED));
+
+       pubsub.get(unparsedMessages)
+               .apply("WriteToDLQ",PubsubIO.writeStrings().to(options.getdlqTopic()));
+
+
         LOG.info("Building pipeline...");
 
         return pipeline.run();
